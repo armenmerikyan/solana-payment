c57f1701041a92c0eec824052fde7e3149f11d43
armenmerikyan <armenmerikyan@yahoo.com>
2025-02-07 21:54:34 -0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,10 @@ module.exports = {
       "stream": require.resolve("stream-browserify"),
       "querystring": require.resolve("querystring-es3"),
       "string_decoder": require.resolve("string_decoder"),
-      "net": false,  // Disable the `net` module (you may need to install this if you're using it)
+      "net": false, // Exclude `net` module
+      "zlib": require.resolve("browserify-zlib"), // Polyfill for `zlib`
+      "async_hooks": false, // Exclude `async_hooks`
+      "vm": require.resolve("vm-browserify"), // Polyfill for `vm`
     },
   },
   module: {
