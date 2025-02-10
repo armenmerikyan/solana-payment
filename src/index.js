ec798c6636dff81aa92ae6d11abf67e91cce8c41
armenmerikyan <armenmerikyan@yahoo.com>
2025-02-10 12:19:45 -0800

    update

--- src/index.js
+++ src/index.js
@@ -34,8 +34,8 @@ document.getElementById("sendSolana").addEventListener("click", async () => {
     try {
         const rpcUrl = 'https://solana-mainnet.g.alchemy.com/v2/brUu7bUWYqnL02KEqM_k1GWoLgTtkGvg'; // Replace with your QuickNode URL
         console.log('RPC URL:', rpcUrl);
-        const connection = new solanaWeb3.Connection(rpcUrl);
-
+        const connection = new Connection(rpcUrl);
+                
         const recipientAddress = "3VSPtEBgfrCHS7UoessBx1FF275Gkw3CeQswR9pCZznS"; // Replace with actual recipient address
         const toPublicKey = new PublicKey(recipientAddress);
 
